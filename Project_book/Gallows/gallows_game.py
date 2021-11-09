a1febbf1b79693a6303ddd32eb899b9a58142fa0
Roman.S <romga123@mail.ru>
2021-11-09 16:38:06 +0300

    доработка

--- Project_book/Gallows/gallows_game.py
+++ Project_book/Gallows/gallows_game.py
@@ -30,17 +30,35 @@ hangman_pic = ["""
  O  |
 /|\ |
 / \ |
-   ==="""]
+   ===""", """
++---+
+[O  |
+/|\ |
+/ \ |   
+   """, """
++---+
+[O] |
+/|\ |
+/ \ |
+   """]
 
-words = "аист акула бабуин баран барсук бобр бык верблюд волк воробей ворон выдра голубь гусь жаба зебра змея " \
-        "индюк кит кобра коза козел койот корова кошка кролик крыса курица лама ласка лебедь лев лиса лосось лось " \
-        "ля-гушка медведь моллюск моль мул муравей мышь норка носорог обезьяна овца окунь олень орел осел панда паук " \
-        "питон попугай пума семга скунс собака сова тигр тритон тюлень утка форель хорек черепаха ястреб".split()
+words = {'Цвета': 'красный оранжевый желтый зеленый синий голубой фиолетовый белый черный коричневый'.split(),
+
+         'Фигуры': 'квадрат треугольник прямоугольник круг эллипс ромб трапеция параллелограмм пятиугольник '
+                   'шестиугольник восьмиугольник'.split(),
+
+         'Фрукты': 'яблоко апельсин лимон лайм груша мандарин виноград грейпфрут персик банан абрикос манго банан '
+                   'нектарин'.split(),
+
+         'Животные': 'аист бабуин баран барсук бык волк зебра кит коза корова кошка кролик крыса лев лиса лось '
+                     'медведь мул мышь норка носорог обезьяна овца олень осел панда пума скунс собака сова тигр '
+                     'тюлень хорек ящерица'.split()}
 
 
-def get_random_word(word_list):
-    word_index = random.randint(0, len(word_list) - 1)
-    return word_list[word_index]
+def get_random_word(word_dict):
+    word_key = random.choice(list(word_dict.keys()))
+    word_index = random.randint(0, len(word_dict[word_key]) - 1)
+    return [word_dict[word_key][word_index], word_key]
 
 
 def display_board(missed_letters, correct_letters, secret_word):
@@ -66,8 +84,7 @@ def display_board(missed_letters, correct_letters, secret_word):
 def get_guess(already_guess):
     while True:
         print("Введите букву.")
-        guess = input()
-        guess = guess.lower()
+        guess = input().lower()
 
         if len(guess) != 1:
             print("Пожалуйста, введите одну букву.")
@@ -81,16 +98,31 @@ def get_guess(already_guess):
 
 def play_again():
     print("Хотите сыграть еще? (Д/Н)")
-    return input().lower().startswith('д',)
+    return input().lower().startswith('д')
 
 
-print ("В И С Е Л И Ц А")
+print("В И С Е Л И Ц А")
+
+difficulty = 'x'
+while difficulty not in ['л', 'с', 'т']:
+    print('Выберите уровень сложности: Л - Легкий, С - Средний, Т - Тяжелый')
+    difficulty = input().lower()
+if difficulty == 'c':
+    del hangman_pic[8]
+    del hangman_pic[7]
+if difficulty == 'т':
+    del hangman_pic[8]
+    del hangman_pic[7]
+    del hangman_pic[5]
+    del hangman_pic[3]
+
 missed_letters = ''
 correct_letters = ''
-secret_word = get_random_word(words)
+secret_word, secret_set = get_random_word(words)
 game_is_done = False
 
 while True:
+    print('Секретное слово из набора: ' + secret_set)
     display_board(missed_letters, correct_letters, secret_word)
 
     guess = get_guess(missed_letters + correct_letters)
@@ -111,6 +143,8 @@ while True:
 
         if len(missed_letters) == len(hangman_pic) - 1:
             display_board(missed_letters, correct_letters, secret_word)
+            print("У вас закончились догадки!\nПосле " + str(len(missed_letters)) + ' упущенные догадки и '
+                  + str(len(correct_letters)) + " правильные догадки, слово было " + secret_word + "'")
             game_is_done = True
 
     if game_is_done:
@@ -118,6 +152,6 @@ while True:
             missed_letters = ''
             correct_letters = ''
             game_is_done = False
-            secret_word = get_random_word(words)
+            secret_word, secret_set = get_random_word(words)
         else:
             break
